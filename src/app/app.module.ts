19eeb7ea9de7ccf9b66b8fb63cb7aaf972f9345b
DenisuDevForFood <denisfeier98@yahoo.com>
2020-04-20 13:58:59 +0300

    add pages and router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,47 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MainNavComponent } from './main-nav/main-nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MyProfileComponent } from './my-profile/my-profile.component';
+import { RestaurantMenuComponent } from './restaurant-menu/restaurant-menu.component';
+import {RouterModule, Routes} from '@angular/router';
+import { LoginPageComponent } from './login-page/login-page.component';
+
+const appRoutes: Routes = [
+  {path: '', redirectTo: 'login', pathMatch: 'full'},
+  {path: 'login', component: LoginPageComponent},
+  {path: 'main', component: MainNavComponent, children: [
+      {path: '', component: MyProfileComponent},
+      {path: 'menu', component: RestaurantMenuComponent}
+    ]},
+  {path: '**', redirectTo: 'login'}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainNavComponent,
+    MyProfileComponent,
+    RestaurantMenuComponent,
+    LoginPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
